af926f9e632117af4d29e18ce2bd74fc5c0d8a3e
Unknown <demolit_fanil@mail.ru>
2024-10-08 13:10:35 +0300

    fixProtect

--- contracts/PriceProtectionV1.sol
+++ contracts/PriceProtectionV1.sol
@@ -181,7 +181,6 @@ contract PriceProtectionV1 {
             }
             
             _executeOrder(_params.orderIds[i], _params.additionalAmount[i], _params.exec[i]);
-            claimOrder(_params.orderIds[i], false);
             unchecked {
                 ++i;
             }
@@ -194,12 +193,11 @@ contract PriceProtectionV1 {
     function _executeOrder(uint256 orderId, uint256 additionalAmount, bool exec) private {
         Order storage order = orders[orderId];
         order.completed = true;
-        
+        balances[order.token][payerAddress] = balances[order.token][payerAddress] + order.amount;
         if (exec) {
             order.additionalAmount = additionalAmount;            
         } else{
-            order.claimed = true;
-            balances[order.token][payerAddress] = balances[order.token][payerAddress] + order.amount;
+            order.claimed = true;            
             emit UpdateBalance(
                 payerAddress,
                 order.token,
@@ -231,8 +229,7 @@ contract PriceProtectionV1 {
             if (msg.sender != order.user) {
                 revert Errors.AvailableOnlyOwner();
             }
-        }
-        balances[order.token][order.user] = balances[order.token][order.user] + order.amount;
+        }        
         order.claimed = true;
         if (msg.sender == order.user) {
             _updateUserActionTime();            
